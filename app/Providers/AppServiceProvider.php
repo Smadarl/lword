893d48d598db6253f328b663075643f98a86dee2
Rob Adams <radams@circlepix.com>
2018-03-29 20:39:13 -0600

    adjust default database string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
         DB::listen(function($query) {
             file_put_contents("/tmp/queries.log", $query->sql . "\n", FILE_APPEND);
         });
+	Schema::defaultStringLength(191);
         //
     }
 
