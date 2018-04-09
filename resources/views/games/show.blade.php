9a3abf6b4afa4ec479787bc85eb301df0120089a
Rob Adams <radams@circlepix.com>
2018-04-09 14:54:33 -0600

    added game list link, auth check on game

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <a href="{{ route('games') }}">Games List</a>
                 <h2>Game with {{ $playerGame->opponent_name }}</h2>
                 <div class="card-header">
 
